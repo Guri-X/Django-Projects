0b2bf68c386f77b414c89b01a0277aaf580e8244
Xan0er <singhhh890.gs@gmail.com>
2021-07-10 11:59:59 +0530

    Removed Secret Key

--- Youtube_Downloader/Youtube_Downloader/settings.py
+++ Youtube_Downloader/Youtube_Downloader/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-)j-*!_%c65j2!^mr4@=9d)l_s)z02jizu)x@!9ecjvhme)lgrm'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
